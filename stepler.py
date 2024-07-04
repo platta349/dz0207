8ee4e3f0a6c780aeb4cd138334def7b822614bde
Плата <bobkovtd10@gmail.com>
2024-07-04 11:48:16 +0300

    буквы иыэа

--- stepler.py
+++ stepler.py
@@ -1,3 +1,6 @@
+
+#https://github.com/platta349/dz0207
+
 text1 = "Питон — мощный язык программирования. Он широко используется в науке о данных."
 text2 = "Питон хорош для анализа данных. Он также используется в веб-разработке."
 symbols = ".,!?:;'\"-—"
@@ -7,20 +10,20 @@ for symbol in symbols:
 text1 = text1.lower()
 text2 = text2.lower()
 
-uniqaln_slova1 = set()
-uniqaln_slova2 = set()
+uniqaln_slova1 = set(text1)
+uniqaln_slova2 = set(text2)
+
+
 print("Уникальные слова в первом тексте:", uniqaln_slova1.difference(uniqaln_slova2))
 print("Уникальные слова во втором тексте:", uniqaln_slova2.difference(uniqaln_slova1))
 
-
-edenstv_slova1 = set()
-edenstv_slova2 = set()
-print("Слова, повторяющиеся один раз за весь текст", edenstv_slova1)
-print("Слова, повторяющиеся один раз за весь текст", edenstv_slova2)
+edenstv_slova1 = set(text1)
+edenstv_slova2 = set(text2)
+print("Слова, повторяющиеся один раз за весь первый текст", edenstv_slova1)
+print("Слова, повторяющиеся один раз за весь второй текст", edenstv_slova2)
 
 obschie_slova = edenstv_slova1.intersection(edenstv_slova2)
 print("Общие слова:", obschie_slova)
-
 
 ednstvennoe = uniqaln_slova1.symmetric_difference(uniqaln_slova2)
 print(f"Слова, которые присутствуют только в одном из текстов:", ednstvennoe)
